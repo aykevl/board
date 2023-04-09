758189d2f5f599e847b35d11873cb022c2695e56
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-04-09 16:01:07 +0200

    all: add smoke tests

---
+++ testdata/smoketest.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/aykevl/board"
+	"github.com/aykevl/tinygl/pixel"
+)
+
+func main() {
+	// Assert that board.Display implements board.Displayer.
+	checkScreen(board.Display.Configure())
+
+	// Assert that Display uses the usual interface.
+	var _ interface {
+		Size() (int16, int16)
+		PhysicalSize() (int, int)
+	} = board.Display
+
+	// Assert that board.Buttons uses the usual interface.
+	var _ interface {
+		Configure()
+		ReadInput()
+		NextEvent() board.KeyEvent
+	} = board.Buttons
+}
+
+func checkScreen[T pixel.Color](display board.Displayer[T]) {
+}
---
+++ tinygo_test.go
@@ -0,0 +1,46 @@
+package board_test
+
+import (
+	"bytes"
+	"flag"
+	"os/exec"
+	"testing"
+)
+
+var boards = []string{
+	// Please keep this list sorted!
+	"gameboy-advance",
+	"gopher-badge",
+	"mch2022",
+	"pybadge",
+	"simulator",
+}
+
+func isXtensa(board string) bool {
+	return board == "mch2022"
+}
+
+var flagXtensa = flag.Bool("xtensa", false, "test Xtensa based boards")
+
+func TestBoards(t *testing.T) {
+	for _, board := range boards {
+		t.Run(board, func(t *testing.T) {
+			if isXtensa(board) && !*flagXtensa {
+				t.Skip("skipping Xtensa board:", board)
+			}
+			outbuf := &bytes.Buffer{}
+			var cmd *exec.Cmd
+			if board == "simulator" {
+				cmd = exec.Command("go", "build", "-o="+t.TempDir()+"/output", "./testdata/smoketest.go")
+			} else {
+				cmd = exec.Command("tinygo", "build", "-o="+t.TempDir()+"/output", "-target="+board, "./testdata/smoketest.go")
+			}
+			cmd.Stderr = outbuf
+			cmd.Stdout = outbuf
+			err := cmd.Run()
+			if err != nil {
+				t.Errorf("failed to compile smoke test: %s\n%s", err, outbuf.String())
+			}
+		})
+	}
+}
