eb67da4ec8f634cccf330f392bf1ff5843953a62
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-06-13 20:00:02 +0200

    pyportal: improve touch screen algorithm
    The new algorithm is mostly based on
    https://dlbeer.co.nz/articles/tsf.html with some modifications.

--- board-pyportal.go
+++ board-pyportal.go
@@ -9,7 +9,6 @@ import (
 	"github.com/aykevl/tinygl/pixel"
 	"tinygo.org/x/drivers"
 	"tinygo.org/x/drivers/ili9341"
-	"tinygo.org/x/drivers/touch"
 	"tinygo.org/x/drivers/touch/resistive"
 )
 
@@ -98,7 +97,12 @@ type touchInput struct{}
 
 var touchID uint32
 
-var lastTouchPoint touch.Point
+// State associated with the touch input.
+var (
+	medianFilterX, medianFilterY medianFilter
+	iirFilterX, iirFilterY       iirFilter
+	lastPosX, lastPosY           int
+)
 
 func (input touchInput) ReadTouch() []TouchPoint {
 	// Values calibrated on the PyPortal I have. Other boards might have
@@ -112,28 +116,52 @@ func (input touchInput) ReadTouch() []TouchPoint {
 	)
 	point := resistiveTouch.ReadTouchPoint()
 	if point.Z > 8192 {
+		medianFilterX.add(point.X)
+		medianFilterY.add(point.Y)
+		var posX, posY int
 		if touchPoints[0].ID == 0 {
+			// First touch on the touch screen.
 			touchID++
 			touchPoints[0].ID = touchID
-		} else {
-			// Use some heuristics to filter out small changes in the pressure
-			// point and to filter out ADC noise. Pretty sure there are much
-			// better algorithms, but this one works well enough for now.
-			// The main problem is that movement isn't smooth but jumpy.
-			const hysteresisX = 2000
-			const hysteresisY = 1300
-			xdiff := point.X - lastTouchPoint.X
-			ydiff := point.Y - lastTouchPoint.Y
-			if xdiff < hysteresisX && xdiff > -hysteresisX {
-				point.X = lastTouchPoint.X
+			for i := 0; i < 4; i++ {
+				// Initialize the median filter at this point with some more
+				// samples, so that the entire median filter is filled.
+				point := resistiveTouch.ReadTouchPoint()
+				medianFilterX.add(point.X)
+				medianFilterY.add(point.Y)
 			}
-			if ydiff < hysteresisY && ydiff > -hysteresisY {
-				point.Y = lastTouchPoint.Y
+			// Reset the IIR filter, and use the position as-is.
+			iirFilterX.add(medianFilterX.value(), true)
+			iirFilterY.add(medianFilterY.value(), true)
+			posX = iirFilterX.value()
+			posY = iirFilterY.value()
+		} else {
+			// New touch value while we were touching before.
+			// Add the value to the IIR filter.
+			iirFilterX.add(medianFilterX.value(), false)
+			iirFilterY.add(medianFilterY.value(), false)
+			// Use some hysteresis to avoid moving the point when it didn't
+			// actually move.
+			posX = lastPosX
+			posY = lastPosY
+			const diff = 400 // arbitrary value that appears to work well
+			if iirFilterX.value() > lastPosX+diff {
+				posX = iirFilterX.value() - diff
+			}
+			if iirFilterX.value() < lastPosX-diff {
+				posX = iirFilterX.value() + diff
+			}
+			if iirFilterY.value() > lastPosY+diff {
+				posY = iirFilterY.value() - diff
+			}
+			if iirFilterY.value() < lastPosY-diff {
+				posY = iirFilterY.value() + diff
 			}
 		}
-		lastTouchPoint = point
-		x := int16(clamp(point.X, ymin, ymax, 0, 239))
-		y := int16(clamp(point.Y, xmin, xmax, 0, 319))
+		lastPosX = posX
+		lastPosY = posY
+		x := int16(clamp(posX, ymin, ymax, 0, 239))
+		y := int16(clamp(posY, xmin, xmax, 0, 319))
 		if display != nil {
 			// Adjust for screen rotation.
 			switch display.Rotation() {
@@ -167,4 +195,63 @@ func clamp(value, lowIn, highIn, lowOut, highOut int) int {
 		valueOut = lowOut
 	}
 	return valueOut
+}
+
+// Touch screen filtering has been implemented using the description in this
+// article:
+// https://dlbeer.co.nz/articles/tsf.html
+// It works a lot better than the rather naive algorithm I implemented before.
+
+type medianFilter [5]int
+
+func (f *medianFilter) add(n int) {
+	// Shift the value into the array.
+	f[0] = f[1]
+	f[1] = f[2]
+	f[2] = f[3]
+	f[3] = f[4]
+	f[4] = n
+}
+
+func (f *medianFilter) value() int {
+	// Optimal sorting algorithm.
+	// It is based on the sorting algorithm described here:
+	// https://bertdobbelaere.github.io/sorting_networks.html
+	sorted := *f
+	compareSwap := func(a, b *int) {
+		if *a > *b {
+			*b, *a = *a, *b
+		}
+	}
+	compareSwap(&sorted[1], &sorted[4])
+	compareSwap(&sorted[0], &sorted[3])
+	compareSwap(&sorted[1], &sorted[3])
+	compareSwap(&sorted[0], &sorted[2])
+	compareSwap(&sorted[2], &sorted[4])
+	compareSwap(&sorted[0], &sorted[1])
+	compareSwap(&sorted[1], &sorted[2])
+	compareSwap(&sorted[3], &sorted[4])
+	compareSwap(&sorted[2], &sorted[3])
+
+	// Return the median value.
+	return sorted[2]
+}
+
+// Infinite impulse response filter, to smooth the input values somewhat.
+type iirFilter struct {
+	state int
+}
+
+func (f *iirFilter) add(x int, reset bool) {
+	if reset {
+		f.state = x
+	}
+	// For every update, the new value is half of x and half of the old value,
+	// added together:
+	//   f.state = f.state*0.5 + x*0.5
+	f.state = (f.state + x + 1) / 2
+}
+
+func (f *iirFilter) value() int {
+	return f.state
 }
